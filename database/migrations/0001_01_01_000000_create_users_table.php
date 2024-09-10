2a37de05a8c212d37ba4bb1223f754723e11d904
Miftah Rahmat <jonathanavena16@gmail.com>
2024-09-10 10:53:09 +0700

    default role

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
-            $table->string('role');
+            $table->string('role')->default('guest');
             $table->string('avatar');
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
