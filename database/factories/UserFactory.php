3ec7991578394117af7a1fd233f8f26dd115d7f7
Miftah Rahmat <jonathanavena16@gmail.com>
2024-09-11 00:04:59 +0700

    update faker

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -2,9 +2,9 @@
 
 namespace Database\Factories;
 
-use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
+use Faker\Factory;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\User>
